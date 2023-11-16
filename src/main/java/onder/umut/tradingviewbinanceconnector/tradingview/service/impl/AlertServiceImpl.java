dfd86d60b4d2b4c8838bebd5cf23be4c6669802f
UmutEmreOnder <umutemreonder@gmail.com>
2023-11-17 00:45:48 +0300

    Added communication between AlertServiceImpl with TradeService

--- src/main/java/onder/umut/tradingviewbinanceconnector/tradingview/service/impl/AlertServiceImpl.java
+++ src/main/java/onder/umut/tradingviewbinanceconnector/tradingview/service/impl/AlertServiceImpl.java
@@ -2,6 +2,7 @@ package onder.umut.tradingviewbinanceconnector.tradingview.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import onder.umut.tradingviewbinanceconnector.binance.trade.service.TradeService;
 import onder.umut.tradingviewbinanceconnector.tradingview.dto.Alert;
 import onder.umut.tradingviewbinanceconnector.tradingview.service.AlertService;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class AlertServiceImpl implements AlertService {
+    private final TradeService tradeService;
     private String lastAction = "";
 
     @Override
@@ -18,12 +20,15 @@ public class AlertServiceImpl implements AlertService {
 
         if (lastAction.equals(alert.getAction())) {
             log.info("Skipping alert because last action '{}' is the same as current action '{}'", lastAction, alert.getAction());
+            return;
         }
 
         lastAction = alert.getAction();
+        tradeService.closeAllPositions();
+
         switch (alert.getAction()) {
-            case "buy" -> log.info("Executing buy action for alert: {}", alert);
-            case "sell" -> log.info("Executing sell action for alert: {}", alert);
+            case "buy" -> tradeService.openLongPosition(alert.getSymbol());
+            case "sell" -> tradeService.openShortPosition(alert.getSymbol());
             default -> log.error("Unknown action '{}' for alert: {}", alert.getAction(), alert);
         }
     }
