567358989e2c851e7519772a834b25c9c2147cf6
UmutEmreOnder <umutemreonder@gmail.com>
2023-11-16 19:46:26 +0300

    Initial commit

---
+++ src/main/java/onder/umut/tradingviewbinanceconnector/TradingViewBinanceConnectorApplication.java
@@ -0,0 +1,13 @@
+package onder.umut.tradingviewbinanceconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingViewBinanceConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingViewBinanceConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onder/umut/tradingviewbinanceconnector/TradingViewBinanceConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package onder.umut.tradingviewbinanceconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingViewBinanceConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
